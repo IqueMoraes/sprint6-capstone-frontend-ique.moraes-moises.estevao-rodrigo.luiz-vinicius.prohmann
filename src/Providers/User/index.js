0ecbc09c4c9d77f0adb85da857e96bccf6b46e51
vinicius <vinapro91@gmail.com>
2021-11-11 13:26:38 -0300

    feat : created register form

--- src/Providers/User/index.js
+++ src/Providers/User/index.js
@@ -1,53 +1,53 @@
 import { createContext, useContext, useState } from "react";
 import { useHistory } from "react-router";
-import api from "../../Services";
+import { api } from "../../Services";
 import { toast } from "react-toastify";
 
-
-const UserContext = createContext({})
+const UserContext = createContext({});
 
 export const UserProvider = ({ children }) => {
+  const [authToken, setAuthToken] = useState(
+    () => localStorage.getItem("@tm/token") || ""
+  );
 
-    const [authToken, setAuthToken] = useState(
-        () => localStorage.getItem("@tm/token") || ""
-    );
+  const history = useHistory();
 
-    const history = useHistory()
+  const handleRegister = (data) => {
+    api
+      .post("/users", data)
+      .then((_) => {
+        toast.success("Cadastro feito com sucesso.");
+        history.push("/");
+      })
+      .catch((_) => toast.error("Email já cadastrado!"));
+  };
 
-    const handleRegister = (data) => {
-        api
-            .post("/users", data)
-            .then(_ => {
-                toast.success("Cadastro feito com sucesso.")
-                history.push("/")
-            })
-            .catch(_ => toast.error("Email já cadastrado!"))
-    }
+  const handleLogin = (data) => {
+    api
+      .post("/login", data)
+      .then((res) => {
+        const token = res.data.accessToken;
+        window.localStorage.clear();
+        window.localStorage.setItem("@tm/token", token);
+        setAuthToken(token);
+        history.push("/dashboard");
+      })
+      .catch((_) => toast.error("Email ou senha incorretos."));
+  };
 
-    const handleLogin = (data) => {
-        api
-            .post("/login", data)
-            .then(res => {
-                const token = res.data.accessToken
-                window.localStorage.clear()
-                window.localStorage.setItem("@tm/token", token)
-                setAuthToken(token)
-                history.push("/dashboard")
-            })
-            .catch(_ => toast.error("Email ou senha incorretos."))
-    }
+  const handleLogout = () => {
+    window.localStorage.clear();
+    setAuthToken("");
+    history.push("/");
+  };
 
-    const handleLogout = () => {
-        window.localStorage.clear()
-        setAuthToken("")
-        history.push("/")
-    }
+  return (
+    <UserContext.Provider
+      value={{ authToken, handleLogin, handleLogout, handleRegister }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
 
-    return (
-        <UserContext.Provider value={{ authToken, handleLogin, handleLogout, handleRegister }} >
-            {children}
-        </UserContext.Provider>
-    )
-}
-
-export const useUser = () => useContext(UserContext)
+export const useUser = () => useContext(UserContext);
--- src/Services/index.js
+++ src/Services/index.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: ""
-})
-
-export default api
+export const api = axios.create({
+  baseURL: "https://capstone-backend-tchau-mamae.herokuapp.com/",
+});
