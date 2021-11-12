b8f7dfb31edd60cd88e5a6cb64320c5ad259054f
moisesgaioli <80260485+moisesgaioli@users.noreply.github.com>
2021-11-11 21:51:35 -0300

    Update index.js

--- src/Components/FormRoutine/index.js
+++ src/Components/FormRoutine/index.js
@@ -22,53 +22,56 @@ export const FormRoutine = () => {
   }
 
 
-  return (
-    <div>
-      <div className="inputContainer">
-        <input
-              placeholder="Nome"
-              value={name}
-              type="text"
-              onChange={e => setName(e.target.value)}
-              required
-          />
-          <input
-              placeholder="Categoria"
-              value={category}
-              type="text"
-              onChange={e => setCategory(e.target.value)}
-              required
-          />
-          <button onClick={createRoutines}> Criar </button>
-      </div>
-      {
-        userRotines.map((item) => {
-          return <div key={item.id}>
-              <p> {item.name} </p>
-              <p> {item.category} </p>
-              <button onClick={()  => setShowFormEdit(true)}> Editar </button>
-              <button onClick={() => deleteRoutine(item.id)}> deletar </button>
-              {
-                showFormEdit &&
-                <div className="inputContainer">
-                <input
-                      placeholder="Nome"
-                      value={name}
-                      type="text"
-                      onChange={e => setName(e.target.value)}
-                      required
-                  />
-                  <input
-                      placeholder="Categoria"
-                      value={category}
-                      type="text"
-                      onChange={e => setCategory(e.target.value)}
-                      required
-                  />
-                  <button onClick={() => handleEdit(item.id)}> editar </button>
-              </div>
-              }
-            </div>
-        })
-      }
-};
+  <div>
+      		<div className="inputContainer">
+				<input
+					placeholder="Nome"
+					value={name}
+					type="text"
+					onChange={e => setName(e.target.value)}
+					required
+				/>
+				<input
+					placeholder="Categoria"
+					value={category}
+					type="text"
+					onChange={e => setCategory(e.target.value)}
+					required
+				/>
+				<button onClick={createRoutines}> Criar </button>
+      		</div>
+
+			  {
+				  userRotines.map(item => {
+					  return <div key={item.id}>
+							<p> {item.name} </p>
+							<p> {item.category} </p>
+							<button onClick={()  => setShowFormEdit(true)}> Editar </button>
+							<button onClick={() => deleteRoutine(item.id)}> deletar </button>
+
+							{
+								showFormEdit && 
+									<div className="inputContainer">
+										<input
+											placeholder="Nome"
+											value={name}
+											type="text"
+											onChange={e => setName(e.target.value)}
+											required
+										/>
+										<input
+											placeholder="Categoria"
+											value={category}
+											type="text"
+											onChange={e => setCategory(e.target.value)}
+											required
+										/>
+										<button onClick={() => handleEdit(item.id)}> editar </button>
+									</div>	
+							}
+					  </div>
+				  })
+			  }
+		</div>
+	)
+}
