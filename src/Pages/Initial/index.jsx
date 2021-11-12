d103636469f9daee174f3a41af41121adfca4644
rodrigo luiz <rodrigoluizandradedeoliveira@gmail.com>
2021-11-12 19:31:22 -0300

    feat: structured page initial

---
+++ src/Pages/Initial/index.jsx
@@ -0,0 +1,134 @@
+const Initial = () => {
+  return (
+    <main>
+      <section>
+        <figure>
+          <img src="#" alt="#" />
+          <figcaption>#</figcaption>
+        </figure>
+
+        <div>
+          <h1>Tchau Mamãe</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+            tempore voluptatibus error iste debitis tenetur omnis veniam saepe
+            odio a velit est reiciendis illo facilis distinctio nesciunt
+            molestias unde laboriosam!
+          </p>
+        </div>
+      </section>
+
+      <article>
+        <nav>
+          <ul>
+            <li> Introdução</li>
+            <li> Creditos</li>
+            <li> Começar</li>
+          </ul>
+        </nav>
+
+        <section>
+          <div>
+            <h3>Sobre o aplicativo</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem obcaecati eveniet quae excepturi provident
+              consectetur quis, quidem cum ea, magnam deleniti quisquam expedita
+              voluptatibus ad amet recusandae, veritatis unde nam.
+            </p>
+          </div>
+
+          <div>
+            <figure>
+              <img src="#" alt="#" />
+              <figcaption>#</figcaption>
+            </figure>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <figure>
+              <img src="#" alt="#" />
+              <figcaption>#</figcaption>
+            </figure>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto culpa, ad sed expedita a sit atque temporibus excepturi
+              vel fugiat quae ipsa, deserunt nam perspiciatis quam possimus
+              quibusdam eaque ea.
+            </p>
+          </div>
+
+          <div>
+            <figure>
+              <img src="#" alt="#" />
+              <figcaption>#</figcaption>
+            </figure>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto culpa, ad sed expedita a sit atque temporibus excepturi
+              vel fugiat quae ipsa, deserunt nam perspiciatis quam possimus
+              quibusdam eaque ea.
+            </p>
+          </div>
+
+          <div>
+            <figure>
+              <img src="#" alt="#" />
+              <figcaption>#</figcaption>
+            </figure>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto culpa, ad sed expedita a sit atque temporibus excepturi
+              vel fugiat quae ipsa, deserunt nam perspiciatis quam possimus
+              quibusdam eaque ea.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <h2>Quem Somos</h2>
+
+          <section>
+            <div>
+              <figure>
+                <img src="#" alt="#" />
+                <figcaption>#</figcaption>
+              </figure>
+            </div>
+
+            <div>
+              <figure>
+                <img src="#" alt="#" />
+                <figcaption>#</figcaption>
+              </figure>
+            </div>
+
+            <div>
+              <figure>
+                <img src="#" alt="#" />
+                <figcaption>#</figcaption>
+              </figure>
+            </div>
+          </section>
+        </section>
+
+        <section>
+          <h2>Começe Agora</h2>
+          <p>Faça login ou cadastre-se para começar agora</p>
+
+          <section>
+            <button>Login</button>
+            <button>Cadastro</button>
+          </section>
+        </section>
+      </article>
+    </main>
+  );
+};
+
+export default Initial;
