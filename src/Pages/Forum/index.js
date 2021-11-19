fd7fc83c06087932ab375ae8d4b5f9928fcdb091
rodrigo luiz <rodrigoluizandradedeoliveira@gmail.com>
2021-11-19 03:24:44 -0300

    fix: style forum

--- src/Pages/Forum/index.js
+++ src/Pages/Forum/index.js
@@ -1,43 +1,65 @@
-import { Box, Heading, Divider } from "@chakra-ui/react"
-import { CardTopics } from "../../Components/FormForum/CardTopics"
-import { ModalCreateTopic } from "../../Components/FormForum/ModalCreateTopic"
-import { Input } from "../../Components/input"
-import { useForum } from "../../Providers/Forum"
-import { FiSearch } from "react-icons/fi"
+import { Box, Heading, Divider } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/layout";
+import { CardTopics } from "../../Components/FormForum/CardTopics";
+import { ModalCreateTopic } from "../../Components/FormForum/ModalCreateTopic";
+import { Input } from "../../Components/input";
+import { useForum } from "../../Providers/Forum";
+import { FiSearch } from "react-icons/fi";
+import { BoxMain } from "../../styles/globalStyle";
 
 export const Forum = () => {
+  const { setSearchTopic } = useForum();
 
-    const { setSearchTopic }= useForum()
+  return (
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="flex-end"
+      p="0 10px"
+    >
+      <BoxMain>
+        <Flex
+          w="100%"
+          h={["40%", "35%"]}
+          alignItems="center"
+          justifyContent="space-around"
+          position="relative"
+          flexDirection={["column", "row"]}
+          borderBottom="2px solid #e3e3e350"
+          mb="10px"
+        >
+          <Flex
+            h="100%"
+            alignItems="center"
+            justifyContent="space-evenly"
+            flexDirection={["column", "column", "column", "row", "row"]}
+          >
+            <Heading as="h1" color="#FEA800" m="0 10px" fontSize="46px">
+              Mural
+            </Heading>
+            <ModalCreateTopic />
+          </Flex>
+          <Flex h="100%" alignItems="center">
+            <Input
+              onChange={(e) => setSearchTopic(e.target.value)}
+              placeholder="Buscar por categoria"
+              w="250px"
+              icon={FiSearch}
+            />
+          </Flex>
+        </Flex>
 
-    return (
-
-            <Box bg="white" maxHeight="90vh" overflow="scroll" margin="4" borderRadius="20px">  
-                <Box display="flex" w="100%" p="5" alignItems={["flex-start", "flex-start", "center"]} flexDirection={["column", "column", "row"]} position="relative">
-                    <Heading as="h1" color="#FEA800" mr={["0", "0", "30px", "200px"]} ml="20px" fontSize="60px"> Mural </Heading>
-                    <ModalCreateTopic />
-                    <Box 
-                        p="2" 
-                        display={["flex"]} 
-                        flexDirection={["column", "column", "row"]} 
-                        position={["static","absolute","static","absolute"]} 
-                        right={["0px"]} 
-                        mr="20px"
-                    >
-                        <Input
-                            onChange={e => setSearchTopic(e.target.value)}
-                            placeholder="Buscar por categoria"
-                            w="250px"
-                            icon={FiSearch}
-                        />
-                    </Box>
-                </Box>
-                
-                <Divider orientation="horizontal" border="5px solid" w="97%" m="0 auto" />
-                
-                <Box borderRadius="20px">
-                    <CardTopics />
-                </Box>
-            </Box>
- 
-    )
-}  
+        <Box
+          borderRadius="20px"
+          w="100%"
+          h={["60%", "65%"]}
+          overflow="auto"
+          pb="5px"
+        >
+          <CardTopics />
+        </Box>
+      </BoxMain>
+    </Flex>
+  );
+};
